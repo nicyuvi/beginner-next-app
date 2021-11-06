babc0246e446c91f44054106e173f2f3ad98715d
Nicolis Yuvienco <nickyuvienco@gmail.com>
2021-11-06 02:07:10 -0700

    add changes

---
+++ src/common/types.ts
@@ -0,0 +1,7 @@
+export default interface GetCatAPI {
+    id:      number;
+    url:     string;
+    webpurl: string;
+    x:       number;
+    y:       number;
+}
---
+++ src/pages/hello.tsx
@@ -0,0 +1,5 @@
+const HelloPage = (props: any) => {
+return <h1>Hello Page</h1>
+}
+
+export default HelloPage
